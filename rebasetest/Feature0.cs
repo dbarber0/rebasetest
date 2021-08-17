3cc7c288080fdf16a762c455ff78eb73fdeb9b30
dennis <dennis.barber@microfocus.com>
2021-08-17 17:35:30 -0400

    Feature0-Commit0

---
+++ rebasetest/Feature0.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rebasetest
+{
+    class Feature0
+    {
+        public void Method0()
+        {
+
+        }
+    }
+}
