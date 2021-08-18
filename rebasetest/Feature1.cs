980c1566d33d3fad3be9627128ecacc5d8e7d23d
dennis <dennis.barber@microfocus.com>
2021-08-18 09:34:59 -0400

    Second commit of Feature1

---
+++ rebasetest/Feature1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rebasetest
+{
+    class Feature1
+    {
+        public void Method0()
+        {
+            //  Added as part of first commit to branch Feature1
+        }
+    }
+}
