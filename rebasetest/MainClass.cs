caa8ef18f9d94e1ad8ca8e41d62615a07d8c216b
dennis <dennis.barber@microfocus.com>
2021-10-08 15:09:29 -0400

    Really return to a known base state

--- rebasetest/MainClass.cs
+++ rebasetest/MainClass.cs
@@ -5,23 +5,6 @@ namespace rebasetest
     {
         public void Method0()
         {
-            //  Added in the intial commit after project creation
-            //  Modified after Bob Commit0
-        }
-
-        public void Method1()
-        {
-            //  Added after Feature0 branched from Main
-        }
-
-        public void Method2()
-        {
-            //  Added after Feature1 branched from Main
-        }
-
-        public void Method3()
-        {
-            //  Added by bob/dbarber042 after Merge Request1
         }
     }
 }
